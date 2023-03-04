e6c82f2d893c8367151f0cb52f16a3f2e0e01fd9
joileJr <joilejunior2@gmail.com>
2023-03-03 21:44:32 -0300

    algumas funções

--- bin/projeto_dart.dart
+++ bin/projeto_dart.dart
@@ -1,5 +1,11 @@
-import 'package:projeto_dart/projeto_dart.dart' as projeto_dart;
+import 'package:projeto_dart/arrowFunction.dart';
+import 'package:projeto_dart/parametroFuction.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${projeto_dart.calculate()}!');
+  show();
+  verificarAprovacao(7, 7, Media);
+}
+
+double Media(int n1, int n2) {
+  return (n1 + n2) / 2;
 }
---
+++ lib/arrowFunction.dart
@@ -0,0 +1,1 @@
+show() => print('Hello world');
