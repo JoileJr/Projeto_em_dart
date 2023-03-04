147312d53922874b5be3f7b1a83833db2fc90b94
joileJr <joilejunior2@gmail.com>
2023-03-03 22:11:22 -0300

    funções nomeadas opcionais

--- bin/projeto_dart.dart
+++ bin/projeto_dart.dart
@@ -1,9 +1,13 @@
 import 'package:projeto_dart/arrowFunction.dart';
 import 'package:projeto_dart/parametroFuction.dart';
+import 'package:projeto_dart/pon.dart';
 
 void main(List<String> arguments) {
   show();
+  var a = multiplicacao(2, 2);
+  print(a);
   verificarAprovacao(7, 7, Media);
+  funcaoParametroPosicionadoOpcional("Ola ", "Joile", 2);
 }
 
 double Media(int n1, int n2) {
